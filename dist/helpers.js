20020070ea445056e5dc9e503e8dd8f11f31ed40
Marc Horne <me@marcushorne.com>
2023-04-13 20:33:43 +0100

    update

--- dist/helpers.js
+++
@@ -1,9 +0,0 @@
-import { SYSTEM_MESSAGE } from './consts';
-export const processMessages = ({ channelMessages }) => {
-    const messageList = [{ role: 'system', content: SYSTEM_MESSAGE }];
-    channelMessages.reverse().forEach((message) => {
-        const role = message.author.username === 'The Monkey' ? 'assistant' : 'user';
-        messageList.push({ role, content: message.content });
-    });
-    return messageList;
-};
