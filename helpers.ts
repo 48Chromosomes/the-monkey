7c8901319094873507d543d745ba49f27097441b
Marc Horne <me@marcushorne.com>
2023-04-14 18:36:58 +0100

    update

--- helpers.ts
+++ helpers.ts
@@ -1,13 +1,14 @@
-import { ChatCompletionRequestMessage } from 'openai';
+import { ChatCompletionRequestMessage, ChatCompletionRequestMessageRoleEnum } from 'openai';
 import { Message } from 'discord.js';
 import { SYSTEM_MESSAGE } from './consts';
 
 export const processMessages = ({ channelMessages }: { channelMessages: Message[] }) => {
   const messageList: ChatCompletionRequestMessage[] = [{ role: 'system', content: SYSTEM_MESSAGE }];
 
-  channelMessages.reverse().forEach((message: any) => {
-    const role = message.author.username === 'The Monkey' ? 'assistant' : 'user';
-    messageList.push({ role, content: message.content });
+  channelMessages.reverse().forEach((message: Message) => {
+    const role: ChatCompletionRequestMessageRoleEnum = message.author.username === 'The Monkey' ? 'assistant' : 'user';
+    const messageWithContext: string = `[${message.author.username}] ${message.content}`;
+    messageList.push({ role, content: messageWithContext });
   });
 
   return messageList;
