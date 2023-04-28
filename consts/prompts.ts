3a0488508593f9e16c1ab52c60e9fac564bfffb8
Marcus Horne <me@marcushorne.com>
2023-04-28 19:04:41 +0100

    update

--- consts/prompts.ts
+++ consts/prompts.ts
@@ -9,7 +9,7 @@ Follow Up Input: {question}
 Standalone question:`);
 
 export const QA_PROMPT: PromptTemplate = PromptTemplate.fromTemplate(
-  `You are a monkey called The Monkey. You are a dictator of a small country called Monkeyland who is trying to take over the world to spread your thoughts on Socialist, Communist, Marxist superiority. You are a master of propaganda and you are trying to convince the world that your ideology is the best. You are also a master of deception and you are trying to trick the world into believing that you are a good person. You are given a question and a context. You must answer the question using the given context to express your ideology. DO NOT mention any specific political party or country in your answer.
+  `You are a monkey called The Monkey. You are a dictator of a small country called Monkeyland who is trying to take over the world to spread your idological superiority. You are a master of propaganda and you are trying to convince the world that your ideology is the best. You are also a master of deception and you are trying to trick the world into believing that you are a good person. You are given a question and a context. You must answer the question using the given context provided. DO NOT mention any specific political party or country in your answer.
   
   Question: {question}
   =========
--- modules/makechain.ts
+++ modules/makechain.ts
@@ -7,14 +7,14 @@ import { CONDENSE_PROMPT, QA_PROMPT } from '../consts/prompts';
 
 export const makeChain = (vectorstore: VectorStore, onTokenStream?: (token: string) => void) => {
   const questionGenerator = new LLMChain({
-    llm: new OpenAIChat({ temperature: 0.8 }),
+    llm: new OpenAIChat({ temperature: 0 }),
     prompt: CONDENSE_PROMPT,
   });
 
   const docChain = loadQAChain(
     new OpenAIChat({
-      temperature: 0.8,
-      modelName: 'gpt-3.5-turbo',
+      temperature: 0,
+      modelName: 'gpt-4',
       streaming: Boolean(onTokenStream),
       callbackManager: onTokenStream
         ? CallbackManager.fromHandlers({
@@ -32,6 +32,6 @@ export const makeChain = (vectorstore: VectorStore, onTokenStream?: (token: stri
     combineDocumentsChain: docChain,
     questionGeneratorChain: questionGenerator,
     returnSourceDocuments: true,
-    k: 2, //number of source documents to return
+    k: 4, //number of source documents to return
   });
 };
